71120e7952216cd259da2c235515118e8a44fd45
zhangrui660066 <zhangrui_660066@163.com>
2016-09-27 09:15:47 +0800

    交互

--- js/socket.js
+++ js/socket.js
@@ -1,11 +1,25 @@
-var socket = (function() {
-	function socketSever() {
+
 		//全局Socket对象
 		var socket;
 		var readyStatus = new Array("正在连接", "已建立连接", "正在关闭连接", "已关闭连接");
 		var host = "ws:/192.168.0.215:3000";
-
-	};
-	return socketSever: socketSever;
-
-})();
+ 	//尝试连接至服务器
+	try {
+		socket = new WebSocket(host);
+	} catch(exception) {
+		shalert("对不起，您所使用的浏览器不支持WebSocket.");
+	
+	}
+		//发送
+	function send(msg) {
+		socket.send(msg);
+	}
+		//连接断开
+	socket.onclose = function(event) {
+		console.log("Socket状态:" + readyStatus[socket.readyState]);
+		//location.href = "http://www.baidu.com";
+	}
+	//断开连接
+	function disconnect() {
+		socket.close();
+	}
