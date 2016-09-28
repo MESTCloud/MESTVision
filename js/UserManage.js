7a1cbb15a49b78e303efd45d877eea217032de5d
zhangrui660066 <zhangrui_660066@163.com>
2016-09-28 08:55:39 +0800

    用户管理 角色管理

--- js/UserManage.js
+++ js/UserManage.js
@@ -1,44 +1,7 @@
-/*交互*/
-/*$(function() {
-	$.ajax({
-		url: "../js/json",
-		type: "get",
-		dataType: "json",
-		success: function(data) {
-			$("tbody").html(bindTable(data));
-		}
-	});
-});*/
-/*获取集合*/
-function bindTable(datatable) {
-	if(datatable.length > 0) {
-		var str = "";
-		$.each(datatable, function(index, data) {
-
-			if(parseInt(index) / 2 == 0) {
-				str += "<tr class='gradeX odd' role='row'>"
-			} else {
-				str += "<tr class='gradeX even' role='row'>"
-			}
-			str += " <td><label class='mt-checkbox mt-checkbox-single mt-checkbox-outline'>";
-			str += "<input type='checkbox' class='checkboxes' value='" + index + "' name='check_table'>";
-			str += "<span></span>";
-			str += "</label> </td>";
-			str += "<td class='sorting_1'>" + data["UserName"] + "</td>";
-			str += "<td>" + data["RealName"] + "</td>";
-			str += "<td>" + data["Mobile"] + "</td>";
-			str += "<td>" + data["RoleName"] + "</td>";
-			str += "</tr>";
-
-		});
-
-	}
-
-	return str;
-}
-
+var UserData;
 /*复选框操作*/
 $(function() {
+
 	/*全选 反选*/
 	$("#checkAll").click(
 		function() {
@@ -66,22 +29,29 @@ $(function() {
 	/*删除user_delete*/
 	$("#user_delete").click(function() {
 		//当复选框已经被选中后
-		if(CheckedLength()) {
+		var indexs = CheckedLength("del");
+
+		if(indexs) {
 			shconfirm("确定要删除吗", function(result) {
+				var id = "";
 				if(result) {
-					/* 对删除进行交互;*/
+					var jsStr = "DeleteUser {\"id\":\"";
+					for(var i = 0; i < indexs.length; i++) {
+						jsStr += UserData[indexs[i]]["Id"] + "\,";
+
+					}
+					jsStr = jsStr.substring(0, jsStr.length - 1) + "\"";
+					jsStr += "}";
+
 				}
+				
+				send(jsStr);
+
 			});
 		}
 	});
-	$("#del_user").click(function() {
-		shalert("删除操作");
 
-	});
 
-	$("#check_cancel").click(function() {
-		$("#User_Check").hide();
-	});
 	/*修改*/
 	$("#user_update").click(function() {
 		//当复选框已经被选中后
@@ -89,61 +59,139 @@ $(function() {
 			$("#user_update").prop("data-toggle", "modal");
 			$('#myModal_Update').modal('show');
 			var idIndex = $("input[name='check_table']:checked").val();
-			console.log(idIndex);
-			send("userlist");
 
-			//收到消息
-			socket.onmessage = function(msg) {
-				var result = msg.data;
-				result = JSON.parse(result);
-				if(result["error"]) {
-					shalert(result["error"]);
-				} else if(result["exception"]) {
-					shalert(result["exception"]);
-				} else {
-					switch(result["Function"]) {
-						case "UserList":
-							var obj=result["data"][idIndex];
-                               $("#login_Update").val(obj["UserName"]);
-                               $("#name_Update").val(obj["RealName"]);
-                               $("#inputphone_Update").val(obj["Mobile"]);
-                               $("#select_role").find("option[text='"+obj['RoleName']+"']").attr("selected",true);
-                               UpdateUser(obj["Id"]);
-                               
-							break;
+			//send("userlist");
+			var obj = UserData[idIndex];
+			$("#login_Update").val(obj["UserName"]);
+			$("#name_Update").val(obj["RealName"]);
+			$("#inputphone_Update").val(obj["Mobile"]);
+			$("#select_role").find("option[text='" + obj['RoleName'] + "']").attr("selected", true);
+			UpdateUser(obj["Id"]);
+		}
 
-					}
+	});
+	/*修改*/
+	function UpdateUser(userid) {
+		$("#save_Update").click(function() {
+			if($("#login_Update").val().trim() == "") {
+				shalert("登录名不能为空！");
+				$("#login_Update").focus();
+				return false;
+			}
+			if($("#name_Update").val().trim() == "") {
+				shalert("姓名不能为空！");
+				$("#name_Update").focus();
+				return false;
+			}
+			if($("#inputphone_Update").val().trim() == "") {
+				shalert("手机号不能为空！");
+				$("#inputphone_Update").focus();
+				return false;
+			} else {
 
+				var phone = document.getElementById('inputphone_Update').value;
+				if(!(/^1(3|4|5|7|8)\d{9}$/.test(phone))) {
+					shalert("手机号码有误，请重填");
+					$("#inputphone_Update").focus();
+					return false;
 				}
 			}
+			if($("#inputPassword_Update").val().trim() == "") {
+				shalert("密码不能为空！");
+				$("#inputPassword_Add").focus();
+				return false;
+			}
+			if($("#inputPassword_Update2").val().trim() == "") {
+				shalert("请再次填写密码！");
+				$("#inputPassword2_Add").focus();
+				return false;
+			}
 
-		}
+			if($("#inputPassword_Add").val().trim() != $("#inputPassword2_Add").val().trim()) {
+				shalert("两次密码不一致，请重新填写");
+				$("#inputPassword2_Add").val("");
+				$("#inputPassword2_Add").focus();
+				return false;
+			}
 
-	});
+			var jsStr = "UpdateUser {\"id\":\"" + userid + "\",\"realname\":\"" + $("#name_Update").val().trim() + "\",\"password\":\"" + $("#inputPassword_Update").val().trim() + "\",\"mobile\":\"" + $("#inputphone_Update").val().trim() + "\",\"role\":\"" + $("#select_role_update").val().trim() + "\"}";
+
+			send(jsStr);
+
+		});
+
+	}
+
 	/*密码重置*/
 	$("#user_password").click(function() {
-		if(CheckedLength()) {
+		var idIndes = CheckedLength("updete");
+		if(idIndes) {
 			$("#user_password").prop("data-toggle", "modal");
-			$('#myModal_PassWordUpdate').modal('show')
+			$('#myModal_PassWordUpdate').modal('show');
+			/*var idIndex = $("input[name='check_table']:checked").val();
+			var obj = UserData[idIndex];
+			PassWordUpdate(obj["Id"]);*/
+			var jsStr = '';
+			for(var i = 0; i < idIndes.length; i++) {
+				jsStr += UserData[idIndes[i]]["Id"] + "\,";
+			}
+			jsStr = jsStr.substring(0, jsStr.length - 1) + "\"";
+
+			PassWordUpdate(jsStr);
+
 		}
 	});
+
+	/*密码重置*/
+	function PassWordUpdate(ids) {
+
+		$("#save_inputPassWordUpdate").click(function() {
+			if($("#inputPassWordUpdate").val().trim() == "" || $("#inputPassWordUpdate2").val().trim() == "") {
+				shalert("密码不能为空");
+				return false;
+			}
+			if($("#inputPassWordUpdate").val().trim() != $("#inputPassWordUpdate2").val().trim()) {
+				shalert("两次密码不一致，请重新填写");
+				$("#inputPassWordUpdate2").val("");
+				$("#inputPassWordUpdate2").focus();
+				return false;
+			}
+			var jsonStr = "SetPassword {\"id\":\"" + ids + "\,\"password\":\"" + $("#inputPassWordUpdate").val().trim() + "\"}";
+			console.log(jsonStr);
+			send(jsonStr);
+		});
+	}
+
 	/*对全选项的判定*/
 	function CheckedLength() {
+
 		var oChecked = document.getElementsByName("check_table");
 		var total = 0;
+		var checked = [];
 		for(var i = 0; i < oChecked.length; i++) {
 			if(oChecked[i].checked) {
 				total++;
+				checked.push(oChecked[i].value);
 			}
 		}
-		if(total == 0) {
-			shalert('请选择操作项！');
-			return false;
-		} else if(total != 1) {
-			shalert('请选择一项进行操作！');
-			return false;
+
+		if(arguments.length > 0) {
+			if(total == 0) {
+				shalert('请选择操作项！');
+				return false;
+			} else {
+				return checked;
+			}
 		} else {
-			return true;
+			if(total == 0) {
+				shalert('请选择操作项！');
+				return false;
+			} else if(total != 1) {
+				shalert('请选择一项进行操作！');
+				return false;
+			} else {
+				return true;
+			}
 		}
 
 	}
@@ -196,121 +244,61 @@ $(function() {
 			return false;
 		}
 		var jsStr = "AddUser {\"username\":\"" + $("#login_Add").val().trim() + "\",\"realname\":\"" + $("#name_Add").val().trim() + "\",\"password\":\"" + $("#inputPassword_Add").val().trim() + "\",\"mobile\":\"" + $("#inputphone_Add").val().trim() + "\",\"role\":\"" + $("#inputRole_Add").val().trim() + "\"}";
-		// "AddUser {\"username\":\"" + $("#login_Add").val().trim() + "\",\"realname\":\"" + $("#name_Add").val().trim() + "\",\"password\":\"" + $("#inputPassword_Add").val().trim() + "\",\"mobile\":\""+ $("#inputphone_Add").val().trim() + "\",\"role\":\"" + $("#check_Add").val().trim() + "\",}";
-		console.log(jsStr);
+
 		send(jsStr);
-
-		socket.onmessage = function(msg) {
-			var result = msg.data;
-			result = JSON.parse(result);
-			if(result["error"]) {
-				shalert(result["error"]);
-			} else if(result["exception"]) {
-				shalert(result["exception"]);
-			} else {
-				switch(result["Function"]) {
-					case "AddUser":
-						shalert("添加成功");
-						return false;
-
-				}
-			}
-		}
-
 	});
-	/*修改*/
-	function UpdateUser(userid)
-	{
-		$("#save_Update").click(function() {
-		if($("#login_Update").val().trim() == "") {
-			shalert("登录名不能为空！");
-			$("#login_Update").focus();
-			return false;
-		}
-		if($("#name_Update").val().trim() == "") {
-			shalert("姓名不能为空！");
-			$("#name_Update").focus();
-			return false;
-		}
-		if($("#inputphone_Update").val().trim() == "") {
-			shalert("手机号不能为空！");
-			$("#inputphone_Update").focus();
-			return false;
-		} else {
 
-			var phone = document.getElementById('inputphone_Update').value;
-			if(!(/^1(3|4|5|7|8)\d{9}$/.test(phone))) {
-				shalert("手机号码有误，请重填");
-				$("#inputphone_Update").focus();
-				return false;
-			}
-		}
-		if($("#inputPassword_Update").val().trim() == "") {
-			shalert("密码不能为空！");
-			$("#inputPassword_Add").focus();
-			return false;
-		}
-		if($("#inputPassword_Update2").val().trim() == "") {
-			shalert("请再次填写密码！");
-			$("#inputPassword2_Add").focus();
-			return false;
-		}
+})
 
-		if($("#inputPassword_Add").val().trim() != $("#inputPassword2_Add").val().trim()) {
-			shalert("两次密码不一致，请重新填写");
-			$("#inputPassword2_Add").val("");
-			$("#inputPassword2_Add").focus();
-			return false;
-		}
+/*获取集合*/
+function bindTable(datatable) {
+	if(datatable.length > 0) {
+		var str = "";
+		$.each(datatable, function(index, data) {
 
-		var jsStr = "UpdateUser {\"Id\":\"" + userid + "\",\"realname\":\"" + $("#name_Add").val().trim() + "\",\"password\":\"" + $("#inputPassword_Add").val().trim() + "\",\"mobile\":\"" + $("#inputphone_Add").val().trim() + "\",\"role\":\"" + $("#inputRole_Add").val().trim() + "\"}";
-		// "AddUser {\"username\":\"" + $("#login_Add").val().trim() + "\",\"realname\":\"" + $("#name_Add").val().trim() + "\",\"password\":\"" + $("#inputPassword_Add").val().trim() + "\",\"mobile\":\""+ $("#inputphone_Add").val().trim() + "\",\"role\":\"" + $("#check_Add").val().trim() + "\",}";
-		console.log(jsStr);
-		send(jsStr);
-
-		socket.onmessage = function(msg) {
-			var result = msg.data;
-			result = JSON.parse(result);
-			if(result["error"]) {
-				shalert(result["error"]);
-			} else if(result["exception"]) {
-				shalert(result["exception"]);
+			if(parseInt(index) / 2 == 0) {
+				str += "<tr class='gradeX odd' role='row'>"
 			} else {
-				switch(result["Function"]) {
-					case "UpdateUser":
-						shalert("修改成功！");
-						return false;
-                     
-				}
+				str += "<tr class='gradeX even' role='row'>"
 			}
-		}
-	});
+			str += " <td><label class='mt-checkbox mt-checkbox-single mt-checkbox-outline'>";
+			str += "<input type='checkbox' class='checkboxes' value='" + index + "' name='check_table'>";
+			str += "<span></span>";
+			str += "</label> </td>";
+			str += "<td class='sorting_1'>" + data["UserName"] + "</td>";
+			str += "<td>" + data["RealName"] + "</td>";
+			str += "<td>" + data["Mobile"] + "</td>";
+			str += "<td>" + data["RoleName"] + "</td>";
+			str += "</tr>";
+
+		});
 
 	}
-	
-	/*密码重置*/
-	$("#save_inputPassWordUpdate").click(function() {
-		if($("#inputPassWordUpdate").val().trim() == "" || $("#inputPassWordUpdate2").val().trim() == "") {
-			shalert("密码不能为空");
-			return false;
-		}
-		if($("#inputPassWordUpdate").val().trim() != $("#inputPassWordUpdate2").val().trim()) {
-			shalert("两次密码不一致，请重新填写");
-			$("#inputPassWordUpdate2").val("");
-			$("#inputPassWordUpdate2").focus();
-			return false;
-		}
-	});
-})
+
+	return str;
+}
+/*角色绑定*/
+function roleBind(data) {
+	var strRole = "";
+	if(data.length > 0) {
+		$.each(data, function(index, item) {
+			strRole += "<option value=" + item["RoleId"] + ">" + item["RealName"] + "</option>";
+		});
+	}
+	console.log(data);
+	return strRole;
+
+}
 
 //连接成功
 socket.onopen = function() {
 	if($.cookie("user") && $.cookie("password")) {
-
 		socket.send("Login {\"username\":\"" + $.cookie("user") + "\",\"password\":\"" + $.cookie("password") + "\"}");
 	}
 
 	send("userlist");
+	send("RoleList");
+
 }
 
 //收到消息
@@ -324,9 +312,31 @@ socket.onmessage = function(msg) {
 	} else {
 		switch(result["Function"]) {
 			case "UserList":
-				console.log(result["data"]);
 
+				UserData = result["data"]
 				$("tbody").html(bindTable(result["data"]));
+				console.log(UserData);
+				break;
+			case "AddUser":
+				shalert("添加成功");
+			/*	var obj={"data":}*/
+				$('#myModal_Add').modal('hide');
+				break;
+			case "UpdateUser":
+				shalert("修改成功");
+				$('#myModal_Update').modal('hide');
+				break;
+			case "DeleteUser":
+				shalert("删除成功");
+				break;
+			case "RoleList":
+				$("#inputRole_Add").html(roleBind(result["data"]));
+				$("#select_role_update").html(roleBind(result["data"]));
+				break;
+
+			case "SetPassword":
+				shalert("密码修改成功！");
+				$('#myModal_PassWordUpdate').modal('hide');
 				break;
 
 		}
