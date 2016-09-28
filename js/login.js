ae84075eb240c00346fd4e186036f58b75620754
zhangrui660066 <zhangrui_660066@163.com>
2016-09-28 08:52:25 +0800

    登录交互

--- js/login.js
+++ js/login.js
@@ -1,26 +1,18 @@
 $(function() {
-	
-  /*密码重置*/
-	$("#user_password").click(function() {
-		
-			$("#user_password").prop("data-toggle", "modal");
-			$('#myModal_PassWordUpdate').modal('show')
-		
-	});
-	/*密码重置*/
-	$("#save_inputPassWordUpdate").click(function() {
-		if($("#inputPassWordUpdate").val().trim() != $("#inputPassWordUpdate2").val().trim()) {
-			shalert("两次密码不一致，请重新填写");
-			$("#inputPassWordUpdate2").val("");
-			$("#inputPassWordUpdate2").focus();
-			return false;
-		}
-	});
-	
+	/*获取地址栏中的值*/
+	if(window.location.search.split('=')[1] == 1) {
+		$.cookie("user", "");
+		$.cookie("password", "");
+	} else {
+		$.cookie("password", "");
+		$("#username").val($.cookie("user"));
+		$("#password").val("");
+	}
+
 	$("#loginClick").on("click", function() {
 		/*判定*/
 
-	if($("#username").val().trim() == "") {
+		if($("#username").val().trim() == "") {
 			shalert('用户名不能为空');
 			return false;
 		}
@@ -32,27 +24,41 @@ $(function() {
 		var jsStr = "Login {\"username\":\"" + $("#username").val().trim() + "\",\"password\":\"" + $("#password").val().trim() + "\"}";
 		send(jsStr);
 
-		socket.onmessage = function(msg) {
-			var result = msg.data;
-			result = JSON.parse(result);
-			if(result["error"]) {
-				shalert(result["error"]);
-			} else if(result["exception"]) {
-				shalert(result["exception"]);
-			} else {
-				switch(result["Function"]) {
-					case "Login":
-						//shalert("Login:" + result["info"]);
-						$.cookie("user", $("#username").val().trim() );
-						$.cookie("password", $("#password").val().trim());
-						window.location.href = "Index.html";
-						break;
-					default:
-						shalert(result);
-						break;
-				}
+	});
+
+
+
+	socket.onmessage = function(msg) {
+		var result = msg.data;
+		result = JSON.parse(result);
+		if(result["error"]) {
+			console.log(result["error"]);
+			shalert(result["error"]);
+		} else if(result["exception"]) {
+			shalert(result["exception"]);
+		} else {
+			switch(result["Function"]) {
+				case "Login":
+					//shalert("Login:" + result["info"]);
+					$.cookie("user", $("#username").val().trim());
+					$.cookie("password", $("#password").val().trim());
+					window.location.href = "Index.html";
+					break;
+				case "ChangePassword":
+
+					if(result["info"].toString().trim() != "") {
+						shalert(result["info"]);
+					} else {
+						shalert("密码修改成功！");
+					}
+
+					$('#myModal_PassWordUpdate').modal('hide');
+					break;
+				default:
+					shalert(result);
+					break;
 			}
 		}
-	});
-   	
+	}
+
 });
