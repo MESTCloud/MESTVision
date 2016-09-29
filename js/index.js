6442a8c8a5ce1472f0999bc35e82067c6193125c
zhangrui660066 <zhangrui_660066@163.com>
2016-09-29 17:10:44 +0800

    菜单管理

--- js/index.js
+++ js/index.js
@@ -1,62 +1,3 @@
-
-$(".sub-menu > li").click(function() {
-	var menu = $('.page-sidebar-menu');
-
-	var el = $(this);
-
-	if(!el || el.size() == 0) {
-		return;
-	}
-
-	if(el.attr('href') == 'javascript:;' ||
-		el.attr('ui-sref') == 'javascript:;' ||
-		el.attr('href') == '#' ||
-		el.attr('ui-sref') == '#'
-	) {
-		return;
-	}
-
-	var slideSpeed = parseInt(menu.data('slide-speed'));
-	var keepExpand = menu.data('keep-expanded');
-
-	// begin: handle active state
-	if(menu.hasClass('page-sidebar-menu-hover-submenu') === false) {
-		menu.find('li.nav-item.open').each(function() {
-			$(this).removeClass('open');
-			$(this).find('> a > .arrow.open').removeClass('open');
-			//$(this).find('> .sub-menu').slideUp();
-		});
-	} else {
-		menu.find('li.open').removeClass('open');
-	}
-
-	menu.find('li.active').removeClass('active');
-	menu.find('li > a > .selected').remove();
-	// end: handle active state
-
-	el.parents('li').each(function() {
-		$(this).addClass('active');
-		$(this).find('> a > span.arrow').addClass('open');
-
-		if($(this).parent('ul.page-sidebar-menu').size() === 1) {
-			$(this).find('> a').append('<span class="selected"></span>');
-		}
-
-		if($(this).children('ul.sub-menu').size() === 1) {
-			$(this).addClass('open');
-		}
-	});
-
-	if($(this).find('> a').attr('data-page')) {
-		$("#ShowPage").attr("src", $(this).find('> a').attr('data-page'));
-	}
-
-	var resBreakpointMd = App.getResponsiveBreakpoint('md');
-	if(App.getViewPort().width < resBreakpointMd && $('.page-sidebar').hasClass('in')) { // close the menu on mobile view while laoding a page 
-		$('.page-header .responsive-toggler').click();
-	}
-});
-
 $("#save_inputPassWordUpdate").click(function() {
 	
 	if($("#inputPassWordUpdateOld").val().trim() == "") {
@@ -84,6 +25,7 @@ socket.onopen = function() {
 		if($.cookie("user") && $.cookie("password")) {
 			socket.send("Login {\"username\":\"" + $.cookie("user") + "\",\"password\":\"" + $.cookie("password") + "\"}");
 		}
+		send("ModuleListByMenu");
 	}
 	//收到消息
 socket.onmessage = function(msg) {
@@ -106,28 +48,11 @@ socket.onmessage = function(msg) {
 
 				$('#myModal_PassWordUpdate').modal('hide');
 				break;
-
-		}
-
-	}
-}
-
-socket.onclose = function(event) {
-	//console.log("Socket状态:" + readyStatus[socket.readyState]);
-	//  location.href = "Login.html";
-}
-
-//发送
-function send(msg) {
-	socket.send(msg);
-}
-
-//断开连接
-function disconnect() {
-	socket.close();
-}
-
-$(".sub-menu > li").click(function () {
+				case "ModuleListByMenu":
+				
+				$("#Menu").html('<li class="sidebar-toggler-wrapper hide"><div class="sidebar-toggler"><span></span></div></li>'+result["data"]);
+				
+				$(".sub-menu > li").click(function () {
     var menu = $('.page-sidebar-menu');
 
     var el = $(this);
@@ -204,4 +129,28 @@ $(".sub-menu > li").click(function () {
         $('.page-header .responsive-toggler').click();
     }
 });
+
+				
+				break;
+
+		}
+
+	}
+}
+
+socket.onclose = function(event) {
+	//console.log("Socket状态:" + readyStatus[socket.readyState]);
+	//  location.href = "Login.html";
+}
+
+//发送
+function send(msg) {
+	socket.send(msg);
+}
+
+//断开连接
+function disconnect() {
+	socket.close();
+}
+
 
