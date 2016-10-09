cb001fccc901ea59f9530a5144c490e131d07837
lillian06 <1946349709@qq.com>
2016-10-09 21:21:29 +0800

    菜单管理添加图标样式，报表页面添加查询功能
    菜单管理添加图标样式，报表页面添加查询功能

--- js/Reporting.js
+++ js/Reporting.js
@@ -1,158 +1,140 @@
+$(function() {
+	/*日历控件响应*/
+	switch(pType) {
+		case "day":
+			$("#divday").show();
+			$("#divmonth").hide();
+			$("#divyear").hide();
+			break;
+		case "month":
+			$("#divmonth").show();
+			$("#divday").hide();
+			$("#divyear").hide();
+			break;
+		case "year":
+			$("#divyear").show();
+			$("#divday").hide();
+			$("#divmonth").hide();
+			break;
+		default:
+			$("#divday").show();
+			$("#divmonth").hide();
+			$("#divyear").hide();
+			break
+	}
 
-$(function () {
-	switch (pType) {
-        case "day":
-            $("#divday").show();
-            $("#divmonth").hide();
-            $("#divyear").hide();
-            $("#divTime").hide();
-            break;
-   		case "month":
-            $("#divmonth").show();
-            $("#divday").hide();
-            $("#divyear").hide();
-            $("#divTime").hide();
-            break;
-        case "year":
-            $("#divyear").show();
-            $("#divday").hide();
-            $("#divmonth").hide();
-            $("#divTime").hide();
-            break;
-        default:
-            $("#divday").hide();
-            $("#divmonth").hide();
-            $("#divyear").hide();
-            $("#divTime").show();
-            break
-    }
-	
-	/*获得Url参数值*/
-	/*function getQueryString(name) {
-	  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-	  var r = window.location.search.substr(1).match(reg);
-
-	  if (r != null) return unescape(r[2]); return null;
-	}*/
-	
-	/*页面title赋值*/
-	/*var ptitle = getQueryString("path").split(",");
-
-	for(var i = 0; i < ptitle.length; i++) {
-		var option = $('<span></span>').html(ptitle[i]);
-		option.appendTo($("#divTitle"));
-
-		if(i < ptitle.length - 1) {
-			$("#divTitle").append("<span>-</span>");
-		}
-	}*/		
-	
 	/*显示excel*/
-	var spread = new GcSpread.Sheets.Spread(document.getElementById('ss'), { sheetCount: 1 });
+	var spread = new GcSpread.Sheets.Spread(document.getElementById('ss'), {
+		sheetCount: 1
+	});
+
+	/*获取当前月的第一天*/
+	function getCurrentMonthFirst(st) {
+		var date = new Date(st);
+		date.setDate(1);
+		return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+	}
 	
-//	/*查询按钮点击事件*/
-//	sheetlist.set("name", "index");
-//  sheetlist.set("searchAttr", "name");
-//  btnQuery.on("click", function () {
-//      var stime = startdate.get("displayedValue") + " " + startime.get("displayedValue");
-//      var etime;
-//      switch (type) {
-//          case "hour":
-//              stime = startdate.get("displayedValue") + " " + startime.get("displayedValue").substring(0, 2) + ":00:00";
-//              etime = startdate.get("displayedValue") + " " + startime.get("displayedValue").substring(0, 2) + ":59:59";
-//              break;
-//          case "day":
-//              etime = startdate.get("displayedValue") + " " + endtime.get("displayedValue");
-//              break;
-//          case "month":
-//              stime = getCurrentMonthFirst(startdate.get("displayedValue")) + " " + startime.get("displayedValue");
-//              etime = getCurrentMonthLast(startdate.get("displayedValue")) + " " + endtime.get("displayedValue");
-//              break;
-//          default:
-//              var etime = enddate.get("displayedValue") + " " + endtime.get("displayedValue");
-//              break
-//      }
-//      if (!string.trim(stime)) {
-//          alert("不能为空！");
-//          return;
-//      }
-//      ViewerStandby.show();
-//      var name = getQueryString("name");
-//      var sheet = getQueryString("sheet");
-//      
-//      if (!!sheetlist.item) {
-//          sheet = sheetlist.item.index;
-//      } else if (!sheet) {
-//          sheet = "0";
-//      }
-//
-//      if (!name) {
-//          alert("无报表参数");
-//          return;
-//      }
-//      console.time("报表耗时:");
-//      xhr.post("ServerCode/ReportingHandler2.ashx?id=" + escape(name) + "&sheet=" + sheet + "&startime=" + stime + "&endtime=" + etime, {
-//          handleAs: "json",
-//          preventCache: true
-//      }).then(function (data) {
-//          if (data.error) {
-//              alert(data.error);
-//          } else {
-//              //fileName = data.filename;
-//              //document.getElementById("pdfViewer").src = "PDFJSInNet/web/viewer.html?file=../../" + data.filename;
-//              //初始化SheetList
-//              //sheetlist.store.setData(data.sheet);
-//              //console.log(data);
-//              
-//              var spread = $("#ss").data("spread");
-//              spread.fromJSON(data);
-//              //updateSheetList();
-//              spreadObj = null;
-//              console.timeEnd("报表耗时:");
-//          }
-//          ViewerStandby.hide();
-//      }, function (error) {
-//          console.log(error);
-//          ViewerStandby.hide();
-//      });
-//  });
-//	
-//	/*导出EXCEL按钮点击事件*/
-//	btnOutputExcel.on("click", function () {
-//    if (fileName) {
-//        ViewerStandby.show();
-//       window.location.href = "ServerCode/DownHandler.ashx?path=" + fileName.replace(".pdf", ".xls");
-//        ViewerStandby.hide();
-//    } else {
-//        alert("无报表导出");
-//    }
-//});
-//
-//	/*导出PDF按钮点击事件*/
-//  btnOutputPdf.on("click", function () {
-//      if (fileName) {
-//          ViewerStandby.show();
-//          window.location.href = "ServerCode/DownHandler.ashx?path=" + fileName;
-//          ViewerStandby.hide();
-//      } else {
-//          alert("无报表导出");
-//      }
-//  });
+	/*获取当前月的最后一天*/
+	function getCurrentMonthLast(st) {
+		var date = new Date(st);
+		var currentMonth = date.getMonth();
+		var nextMonth = ++currentMonth;
+		var nextMonthFirstDay = new Date(date.getFullYear(), nextMonth, 1);
+		var oneDay = 1000 * 60 * 60 * 24;
+		date = new Date(nextMonthFirstDay - oneDay);
+		return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+	}
+
+	/*查询按钮点击事件*/
+	$("#btnQuery").click(function() {
+		
+		/*报表名称*/
+		var pName = getQueryString("name");
+
+		/*开始日期*/
+		var stime;
+
+		/*结束日期*/
+		var etime;
+
+		switch(pType) {
+
+			case "day":
+				var startTime = $("#startTime1").val().trim();
+				var endTime = $("#endTime1").val().trim();
+
+				if(startTime == "") {
+					shalert("开始日期不能为空！");
+					return false;
+				}
+				if(endTime == "") {
+					shalert("结束日期不能为空！");
+					return false;
+				}
+
+				if(startTime != "" && endTime != "" && startTime > endTime) {
+					shalert("结束日期不能大于开始日期！");
+					return false;
+				}
+				stime = $("#startTime1").val();
+				etime = $("#endTime1").val();
+				break;
+			case "month":
+
+				if($("#startTime2").val().trim() == "") {
+
+					shalert("日期不能为空！");
+					return false;
+				}
+				stime = getCurrentMonthFirst($("#startTime2").val()) + " " + "08:00:00";
+				etime = getCurrentMonthLast($("#startTime2").val()) + " " + "08:00:00";
+				break;
+				
+			case "year":
+				if($("#startTime3").val().trim() == "") {
+					shalert("日期不能为空！");
+					return false;
+				}
+				stime = $("#startTime3").val() + "-01-01 08:00:00";
+				etime = $("#startTime3").val() + "-12-31 08:00:00";
+				break;
+			default:
+				stime = $("#startTime1").val();
+				etime = $("#endTime1").val();
+				break;
+		}
+		
+		console.log(stime);
+		console.log(etime);
+
+		var DataST = new Date(stime.replace(/-/g, '/'));
+		var DateS = new Date(DataST);
+
+		var DateEN = new Date(etime.replace(/-/g, '/'));
+		var DateE = new Date(DateEN);
+
+		console.log(DateS);
+		console.log(DateE);
+
+		var jsStr = "Report {\"name\":\"" + pName + "\",\"start\":\"" + stime + "\",\"end\":\"" + etime + "\"}";
+		console.log(jsStr);
+		//send(jsStr);
+
+	});
 });
 
 //连接成功
-/*socket.onopen = function() {
+socket.onopen = function() {
 	if($.cookie("user") && $.cookie("password")) {
 		socket.send("Login {\"username\":\"" + $.cookie("user") + "\",\"password\":\"" + $.cookie("password") + "\"}");
 	}
-
-	send("userlist");
-
-}*/
+}
 
 //收到消息
-/*socket.onmessage = function(msg) {
+socket.onmessage = function(msg) {
 	var result = msg.data;
+
 	result = JSON.parse(result);
 	if(result["error"]) {
 		shalert(result["error"]);
@@ -160,23 +142,10 @@ $(function () {
 		shalert(result["exception"]);
 	} else {
 		switch(result["Function"]) {
-			case "AddUser":
-				shalert("添加成功");
-
-				var obj = {
-					"Id": result["info"],
-					"Mobile": $("#inputphone_Add").val().trim(),
-					"RealName": $("#name_Add").val().trim(),
-					"UserName": $("#login_Add").val().trim(),
-					"RoleName": $("#inputRole_Add  option:selected").text().trim()
-				};
-
-				UserData.push(obj);
-				$("tbody").append(AddUser(obj));
-				$('#myModal_Add').modal('hide');
+			case "Report":
+				console.log(result["data"])
 				break;
 		}
-
 	}
 }
 
@@ -194,5 +163,4 @@ function send(msg) {
 //断开连接
 function disconnect() {
 	socket.close();
-}*/
-
+}
--- js/layout.js
+++ js/layout.js
@@ -3,13 +3,13 @@
 	function getQueryString(name) {
 		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
 		var r = window.location.search.substr(1).match(reg);
-
+         
 		if(r != null) return unescape(r[2]);
 		return null;
 	}
 	
-	var pType=getQueryString("type");
-	//var pType="month";
+	//var pType=getQueryString("type");
+	var pType="day";
 	
 	/*页面title赋值*/
 	var ptitle = getQueryString("path").split(",");
