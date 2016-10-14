7c61d8cf792b2fb46c1b91c895518492c8ca4b7a
lillian06 <1946349709@qq.com>
2016-10-14 14:27:22 +0800

    实时报警页面添加定时刷新功能
    实时报警页面添加定时刷新功能

--- js/RealtimeAlarm.js
+++ js/RealtimeAlarm.js
@@ -1,52 +1,150 @@
-
 var RealTimeAlarmData;
 
 $(document).ready(function() {
-/*二维码的点击事件*/
-	$("#QRcode").on("click",function(){
-	
+		
+	/*二维码的点击事件*/
+	$("#QRcode").on("click", function() {
+
 		$("#div_QRcode").toggle("slow");
 	});
 
 	/*	固定表头*/
-	$("#tblList").freezeHeader();
-	
-	/*全选 反选 模拟量*/
-	$("#checkAll").click(
-		function() {
+	//$("#tblList").freezeHeader();
 
-			if(this.checked) {
+	/*全选 反选*/
+	$("#checkAll").click(function() {
+		if(this.checked) {
 
-				$("input[name='check_table']").prop('checked', true);
+			$("input[name='check_table']").prop('checked', true);
 
-			} else {
-				$("input[name='check_table']").prop('checked', false);
-			}
+		} else {
+			$("input[name='check_table']").prop('checked', false);
 		}
-	);
-	
+	});
+
+	/*单选全选后,全选按钮选中*/
+	$("tbody").bind("click", function() {
+
+		var $check = $("input[name='check_table']:checked");
+		var ototal = $check.length;
+
+		if($("input[name='check_table']").length == ototal) {
+			$("#checkAll").prop("checked", true);
+		} else {
+			$("#checkAll").prop("checked", false);
+		}
+	});
+
 	/*查询按钮点击事件*/
 	$("#btnQuery").click(function() {
-		
-		/*报表名称*/
-		var pUsername = $.cookie("user");
 
+		// 获取指定时间段内实时报警数据
+		sendCheckTimeData();
+
+	});
+
+	/*确认选中按钮点击事件*/
+	$("#btnConfirmationCheck").click(function() {
+		//当复选框已经被选中后
+		indexsDel = CheckedLength("check");
+
+		if(indexsDel) {
+			shconfirm("确认要确认多条报警吗?", function(result) {
+				var id = "";
+				if(result) {
+					var jsStr = "ConfirmMultiAlarmTagInfo {\"id\":\"";
+					for(var i = 0; i < indexsDel.length; i++) {
+
+						jsStr += RealTimeAlarmData[indexsDel[i]]["AlarmID"] + "\,";
+					}
+					jsStr = jsStr.substring(0, jsStr.length - 1) + "\"";
+					jsStr += "}";
+
+				}
+
+				send(jsStr);
+			});
+		}
+	});
+
+	/*全部确认按钮点击事件*/
+	$("#btnConfirmAllAlarmData ").click(function() {
+		//当复选框已经被选中后
+		indexsDel = CheckedLength("check");
+
+		if(indexsDel) {
+			shconfirm("确认要确认所有报警吗?", function(result) {
+				if(result) {
+					send(ConfirmAllAlarmTagInfo);
+				};
+			});
+		};
+	});
+});
+
+// 获取指定时间段内实时报警数据
+function sendCheckTimeData()
+{
 		/*开始日期*/
 		var pStime = $("#startTime").val().trim();
 
 		/*结束日期*/
 		var pEtime = $("#endTime").val().trim();
+		
+		if(pStime == "") {
+			shalert("开始日期不能为空！");
+			return false;
+		}
+		if(pEtime == "") {
+			shalert("结束日期不能为空！");
+			return false;
+		}
 
-		var jsStr = "CheckRealTimeAlarmInfo {\"username\":\"" + pUsername + "\",\"startTime\":\"" + pStime + "\",\"endTime\":\"" + pEtime + "\"}";
-		console.log(jsStr);
+		if(pStime != "" && pEtime != "" && pStime > pEtime) {
+			shalert("结束日期不能大于开始日期！");
+			return false;
+		}
+
+		var jsStr = "CheckRealTimeAlarmInfo {\"username\":\"" + $.cookie("user") + "\",\"startTime\":\"" + pStime + "\",\"endTime\":\"" + pEtime + "\"}";
+console.log(jsStr);
 		send(jsStr);
+}
 
-	});
-	
-	
-});
-	
-/*获取模拟量集合*/
+/*对全选项的判定*/
+function CheckedLength() {
+
+	var oChecked = document.getElementsByName("check_table");
+
+	var total = 0;
+	var checked = [];
+	for(var i = 0; i < oChecked.length; i++) {
+		if(oChecked[i].checked) {
+			total++;
+			checked.push(oChecked[i].value);
+		}
+	}
+
+	if(arguments.length > 0) {
+		if(total == 0) {
+			shalert('请选择操作项！');
+			return false;
+		} else {
+			return checked;
+		}
+	} else {
+		if(total == 0) {
+			shalert('请选择操作项！');
+			return false;
+		} else if(total != 1) {
+			shalert('请选择一项进行操作！');
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
+
+/*获取table集合*/
 function bindTable(datatable) {
 	if(datatable.length > 0) {
 		var str = "";
@@ -54,28 +152,21 @@ function bindTable(datatable) {
 
 			// 结束时间
 			var pEndTime = data["EndTime"] == null ? "" : data["EndTime"];
-			
-			if(data["Severity"] == "高高报")
-			{
+
+			if(data["Severity"] == "高高报") {
 				str += "<tr class='fontred' role='row'>";
-			}
-			else if(data["Severity"] == "高报")
-			{
+			} else if(data["Severity"] == "高报") {
 				str += "<tr class='fontblue' role='row'>";
-			}
-			else if(data["Severity"] == "低报")
-			{
+			} else if(data["Severity"] == "低报") {
 				str += "<tr class='fontviolet' role='row'>";
-			}
-			else if(data["Severity"] == "低低报")
-			{
+			} else if(data["Severity"] == "低低报") {
 				str += "<tr class='fontgreen' role='row'>";
 			}
 			str += " <td><label class='mt-checkbox mt-checkbox-single mt-checkbox-outline'>";
 			str += "<input type='checkbox' class='checkboxes' value='" + index + "' name='check_table'>";
 			str += "<span></span>";
 			str += "</label> </td>";
-			str += "<td>" + data["TagName"]   + "</td>";
+			str += "<td>" + data["TagName"] + "</td>";
 			str += "<td>" + data["Description"] + "</td>";
 			str += "<td>" + data["Severity"] + "</td>";
 			str += "<td>" + data["StartTime"] + "</td>";
@@ -83,79 +174,112 @@ function bindTable(datatable) {
 			str += "<td>" + data["AlarmValue"] + "</td>";
 			str += "<td>" + data["AlarmStandard"] + "</td>";
 			str += "<td>";
-			str += "<button type='button' class='btn btn1 btn-success btnConfirmAlarm'   data-value='"+data["AlarmID"]+"'>";
-			str +="<span>确认报警</span>"			                
-			str +="</button></td>"
+			str += "<button type='button' class='btn btn1 btn-success btnConfirmAlarm'   data-value='" + data["AlarmID"] + "'>";
+			str += "<span>确认报警</span>"
+			str += "</button></td>"
 			str += "</tr>";
 		});
 	}
 
 	return str;
-}	
+}
 
 // 确认按钮点击事件
-function ConfirmAlarmData(pAlarmID)
-{
+function ConfirmAlarmData(pAlarmID) {
 	var jsStr = "ConfirmOneAlarmTagInfo {\"id\":\"" + pAlarmID + "\"}";
 	console.log(jsStr);
 	send(jsStr);
 }
 
-	//连接成功
-	socket.onopen = function() {
-		if($.cookie("user") && $.cookie("password")) {
-			socket.send("Login {\"username\":\"" + $.cookie("user") + "\",\"password\":\"" + $.cookie("password") + "\"}");
-		}
+//连接成功
+socket.onopen = function() {
+	if($.cookie("user") && $.cookie("password")) {
+		socket.send("Login {\"username\":\"" + $.cookie("user") + "\",\"password\":\"" + $.cookie("password") + "\"}");
+	}
+	senddata();
+}
 
-		send("RealTimeAlarmInfo {\"username\":\"" + $.cookie("user") + "\"}");
-	}
-	
-	//收到消息
-	socket.onmessage = function(msg) {
-		var result = msg.data;
-		result = JSON.parse(result);
-		if(result["error"]) {
-			shalert(result["error"]);
-		} else if(result["exception"]) {
-			shalert(result["exception"]);
-		} else {
-			switch(result["Function"]) {
-				case "RealTimeAlarmInfo":
-					RealTimeAlarmData = result["data"]
-					console.log(result["data"]);					
-					$("tbody").html(bindTable(result["data"]));		
-	                 
-	                 /*确认报警按钮点击事件*/
-					$(".btnConfirmAlarm").click(function() {
-						var pAlarmID = this.getAttribute("data-value");
-						console.log(pAlarmID);
-						ConfirmAlarmData(pAlarmID);
+// 获取所有实时报警数据
+function senddata()
+{
+	console.log("RealTimeAlarmInfo {\"username\":\"" + $.cookie("user") + "\"}");
+	send("RealTimeAlarmInfo {\"username\":\"" + $.cookie("user") + "\"}");
+}
+
+//收到消息
+socket.onmessage = function(msg) {
+	var result = msg.data;
+	result = JSON.parse(result);
+	if(result["error"]) {
+		shalert(result["error"]);
+	} else if(result["exception"]) {
+		shalert(result["exception"]);
+	} else {
+		switch(result["Function"]) {
+			case "RealTimeAlarmInfo":
+				RealTimeAlarmData = result["data"];
+				console.log(result["data"]);
+				$("tbody").html(bindTable(result["data"]));
+
+				/*确认报警按钮点击事件*/
+				$(".btnConfirmAlarm").click(function() {
+					var pAlarmID = this.getAttribute("data-value");
+					shconfirm("确认要确认报警吗?", function(result) {
+						if(result) {						
+							console.log(pAlarmID);
+							ConfirmAlarmData(pAlarmID);
+						};
+					});								
+				});
+				break;
+
+			case "CheckRealTimeAlarmInfo":
+				RealTimeAlarmData = result["data"]
+				console.log(result["data"]);
+				$("tbody").html(bindTable(result["data"]));
+
+				/*确认报警按钮点击事件*/
+				$(".btnConfirmAlarm").click(function() {
+					var pAlarmID = this.getAttribute("data-value");
+					shconfirm("确认要确认报警吗?", function(result) {
+						if(result) {
+							var pAlarmID = this.getAttribute("data-value");
+							console.log(pAlarmID);
+							ConfirmAlarmData(pAlarmID);
+						};
 					});
-					break;
-					
-				case "CheckRealTimeAlarmInfo":
-					console.log(result["data"]);					
-					$("tbody").html(bindTable(result["data"]));	
-					
-					/*确认报警按钮点击事件*/
-					$(".btnConfirmAlarm").click(function() {
-						var pAlarmID = this.getAttribute("data-value");
-						console.log(pAlarmID);
-						ConfirmAlarmData(pAlarmID);
-					});
-					
-					break;
-				case "ConfirmOneAlarmTagInfo":
-					shalert("确认成功");
-					break;	
-			}
+				});
+
+				break;
+			case "ConfirmOneAlarmTagInfo":
+				shalert("确认成功");
+				
+				/*开始日期*/
+				var pStime = $("#startTime").val().trim(); 
+				
+				if(pStime == "")
+				{
+					// 获取所有实时报警数据
+					senddata();
+				}
+				else
+				{
+					// 获取指定时间段内实时报警数据
+					sendCheckTimeData();
+				}
+		
+				break;
+			case "ConfirmMultiAlarmTagInfo":
+				shalert("确认成功");
+				break;
 		}
 	}
-	
+}
+
 //连接断开
 socket.onclose = function(event) {
 	console.log("Socket状态:" + readyStatus[socket.readyState]);
-	location.href = "../Login.html";
+	window.parent.location.href = "../Login.html";
 }
 
 //发送
---
+++ js/RealtimeAlarmRefrash.js
@@ -0,0 +1,101 @@
+
+//全局realtimeSocket对象
+var realtimeSocket;
+
+//尝试连接至服务器
+try {
+    realtimeSocket = new WebSocket("ws://36.110.66.3:29001");
+} catch (exception) {
+    shalert("对不起，您所使用的浏览器不支持WebSocket.");
+}
+
+// 页面加载
+$(document).ready(function() {
+	
+	// 刷新周期下拉选单change事件
+	$("#select_Refrash").change(function() {
+	
+	// 刷新秒数
+	var pRefrash = $("#select_Refrash").val();
+	realtimeSocket.send("{\"username\":\"" + $.cookie("user") + "\",\"updatetime\":\"" + pRefrash + "\"}");
+	});	
+});
+
+/*获取table集合*/
+function bindTable(datatable) {
+	if(datatable.length > 0) {
+		var str = "";
+		$.each(datatable, function(index, data) {
+
+			// 结束时间
+			var pEndTime = data["EndTime"] == null ? "" : data["EndTime"];
+
+			if(data["Severity"] == "高高报") {
+				str += "<tr class='fontred' role='row'>";
+			} else if(data["Severity"] == "高报") {
+				str += "<tr class='fontblue' role='row'>";
+			} else if(data["Severity"] == "低报") {
+				str += "<tr class='fontviolet' role='row'>";
+			} else if(data["Severity"] == "低低报") {
+				str += "<tr class='fontgreen' role='row'>";
+			}
+			str += " <td><label class='mt-checkbox mt-checkbox-single mt-checkbox-outline'>";
+			str += "<input type='checkbox' class='checkboxes' value='" + index + "' name='check_table'>";
+			str += "<span></span>";
+			str += "</label> </td>";
+			str += "<td>" + data["TagName"] + "</td>";
+			str += "<td>" + data["Description"] + "</td>";
+			str += "<td>" + data["Severity"] + "</td>";
+			str += "<td>" + data["StartTime"] + "</td>";
+			str += "<td>" + pEndTime + "</td>";
+			str += "<td>" + data["AlarmValue"] + "</td>";
+			str += "<td>" + data["AlarmStandard"] + "</td>";
+			str += "<td>";
+			str += "<button type='button' class='btn btn1 btn-success btnConfirmAlarm'   data-value='" + data["AlarmID"] + "'>";
+			str += "<span>确认报警</span>"
+			str += "</button></td>"
+			str += "</tr>";
+		});
+	}
+
+	return str;
+}
+
+//连接成功
+realtimeSocket.onopen = function () {
+	
+	// 发送消息
+   realtimeSocket.send("{\"username\":\"" + $.cookie("user") + "\",\"updatetime\":\"" + "1000" + "\"}");
+}
+
+//收到消息
+realtimeSocket.onmessage = function (msg) {
+    var result = msg.data;
+    result = JSON.parse(result);
+    if (result["error"]) {
+        console.log(result["error"]);
+    } else if (result["exception"]) {
+        console.log(result["exception"]);
+    } else {
+        switch (result["Function"]) {
+            case "RealTimeAlarmInfo":
+                console.log(result["data"]);
+                $("tbody").html(bindTable(result["data"]));
+                break;
+            default:
+                console.log(result);
+                break;
+        }
+    }
+}
+
+//连接断开
+realtimeSocket.onclose = function (event) {
+    console.log("Socket状态:" + readyStatus[realtimeSocket.readyState]);
+    window.parent.location.href = "../Login.html";
+}
+
+//断开连接
+function disconnect() {
+    realtimeSocket.close();
+}
