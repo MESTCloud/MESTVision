2c59696d2a0fd2e4d8bc10ba97a05dfa77ddf4e1
lillian06 <1946349709@qq.com>
2016-11-03 09:36:03 +0800

    首页添加滚动条自适应
    首页添加滚动条自适应

--- js/indexpage.js
+++ js/indexpage.js
@@ -1,9 +1,14 @@
+/*屏幕高度*/
+var pFrameHeight = $(window).height();
+
+$("#divtable").css("height", pFrameHeight-50);
 
 var oLis=DOM.getElesByClass("tab");//IE6/7/8不支持
 var strEvent="onclick";
 for(var i=0;i<oLis.length;i++){
 	oLis[i][strEvent]=changeTab;//changeTab是不带括号的，表示只是事件绑定，而不是把changeTab的运行结果返回给事件
 }
+
 //首先获取所有的类名，给每个类名一个click'方法，
 function changeTab(){
 	
