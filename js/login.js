90622c48ddabf0506414e19f798ba6eb108a4292
zhangrui660066 <zhangrui_660066@163.com>
2016-12-15 16:48:26 +0800

    登录回车事件

--- js/login.js
+++ js/login.js
@@ -8,12 +8,12 @@ $(function() {
 		$("#username").val($.cookie("user"));
 		$("#password").val("");
 	}
-	document.onkeydown = function(event) {
+	/*document.onkeydown = function(event) {
 		var e = event || window.event || arguments.callee.caller.arguments[0];
 		if(e && e.keyCode == 13) { // enter 键
 			login();
 		}
-	}
+	}*/
 	$("#loginClick").on("click", function() {
 		/*判定*/
 		login();
